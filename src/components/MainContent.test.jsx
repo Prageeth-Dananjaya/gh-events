a8790c20a77c38dbf864ab0e2c2f99702410e0cd
prageeth dananjaya <e19431@eng.pdn.ac.lk>
2024-08-04 10:28:41 +0530

    Change the files

--- src/components/MainContent.test.jsx
+++ src/components/MainContent.test.jsx
@@ -4,7 +4,7 @@ import userEvent from "@testing-library/user-event";
 
 import MainContent from "./MainContent";
 
-// My tests
+// This is a test
 
 describe("MainContent", () => {
   it("should render a button", () => {
